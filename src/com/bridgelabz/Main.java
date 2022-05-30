079e904b7c150ed648038898043de42aaf82dbf8
Surendra234 <surendramahawar873@gmail.com>
2022-05-30 17:55:51 +0530

    Add new Contact

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,15 +1,12 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        AddressBook addressBook = new AddressBook();
         System.out.println("Welcome to Address Book Program ");
-        // creating a contact
-        Contacts contacts1 = new Contacts("surendra","Mahawar","365 gumanpura",
-                "surendramahawar873@gmail.com","kota","Rajasthan",324007,123456781);
-        System.out.println(contacts1);
+        addressBook.addNewContacts();
+        addressBook.showContacts();
     }
 }
